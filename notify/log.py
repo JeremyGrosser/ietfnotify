27d9365a51f1c16b8ad99a845fdd918a5d65c53f
Jeremy Grosser <synack@neohippie.net>
2006-08-08 09:54:58 +0000

    Added functionality for logging to different syslog facilities/priorities

--- notify/log.py
+++ notify/log.py
@@ -6,25 +6,73 @@
 
 import sys
 import syslog
+import notify.config as conf
 
 # Constants
 DEBUG = 'DEBUG'
 ERROR = 'ERROR'
 INFO = 'INFO'
 NETWORK = 'NETWORK'
-debugMode = False
 
-class LogStream:
-	def __init__(self, f):
-		self.f = f
-	def write(self, s):
-		self.f.write(s)
-		self.f.flush()
+
+# ----------------------------------------------------------------------
+# Utility functions
+
+def getconfigfacility():
+    facilitymap = { 
+	"user":     syslog.LOG_USER,
+	"mail":     syslog.LOG_MAIL,
+	"daemon":   syslog.LOG_DAEMON,
+	"auth":     syslog.LOG_AUTH,
+	"lpr":      syslog.LOG_LPR,
+	"news":     syslog.LOG_NEWS,
+	"uucp":     syslog.LOG_UUCP,
+	"cron":     syslog.LOG_CRON,
+	"local0":   syslog.LOG_LOCAL0,
+	"local1":   syslog.LOG_LOCAL1,
+	"local2":   syslog.LOG_LOCAL2,
+	"local3":   syslog.LOG_LOCAL3,
+	"local4":   syslog.LOG_LOCAL4,
+	"local5":   syslog.LOG_LOCAL5,
+	"local6":   syslog.LOG_LOCAL6,
+	"local7":   syslog.LOG_LOCAL7,
+        }
+    try:
+        facility = conf.get("logging", "facility")
+    except:
+        facility = "user"
+    return facilitymap.get(facility, syslog.LOG_USER)
+
+
+def getconfigpriority():
+    #LOG_EMERG, LOG_ALERT, LOG_CRIT, LOG_ERR, LOG_WARNING, LOG_NOTICE,
+    #LOG_INFO, LOG_DEBUG.
+    prioritymap = { 
+#	"user":     syslog.LOG_USER,
+	"emerg":	syslog.LOG_EMERG, 
+	"emergency":	syslog.LOG_EMERG, 
+	"alert":	syslog.LOG_ALERT, 
+	"crit":         syslog.LOG_CRIT, 
+	"critical":	syslog.LOG_CRIT, 
+	"err":          syslog.LOG_ERR, 
+	"error":	syslog.LOG_ERR, 
+	"warn":         syslog.LOG_WARNING, 
+	"warning":	syslog.LOG_WARNING, 
+	"notice":	syslog.LOG_NOTICE, 
+	"info":         syslog.LOG_INFO, 
+	"debug":	syslog.LOG_DEBUG, 
+        }
+    try:
+        priority = conf.get("logging", "priority")
+    except:
+        priority = "info"
+    return prioritymap.get(priority, syslog.LOG_INFO)
+
 
 # ----------------------------------------------------------------------
 
-syslog.openlog("ietfnotify", syslog.LOG_PID, syslog.LOG_USER)
-syslog.setlogmask(syslog.LOG_UPTO(syslog.LOG_INFO))
+syslog.openlog("ietfnotify", syslog.LOG_PID, getconfigfacility())
+syslog.setlogmask(syslog.LOG_UPTO(getconfigpriority()))
 
 def debug(msg):
     syslog.syslog(syslog.LOG_DEBUG, msg)
@@ -48,9 +96,8 @@ def log(pri, msg):
         warn(msg)
     if pri == ERROR:
         err(msg)
-	if debugMode:
-		print msg
 
 def cleanup():
     pass
-    
+
+
